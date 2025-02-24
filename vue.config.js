b287cb6c2cd9cb1a46965fee1b3a6364629f0c24
HallowDem <halloweendemon3418@gmail.com>
2025-02-24 15:51:36 -0500

    Add test data

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,15 @@
-const { defineConfig } = require('@vue/cli-service')
-module.exports = defineConfig({
-  transpileDependencies: true
-})
+const fs = require('fs');
+
+module.exports = {
+  devServer: {
+    onBeforeSetupMiddleware(devServer) {
+      devServer.app.get('/api/goods/home', (req, res) => {
+        fs.readFile('./db/home.json', 'utf8', (err, data) => {
+          if (!err) {
+            res.json(JSON.parse(data));
+          }
+        })
+      });
+    }
+  }
+}
