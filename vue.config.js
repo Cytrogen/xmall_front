3621a517ed5da7781ca7f735317b56f4d6bdd2f9
HallowDem <halloweendemon3418@gmail.com>
2025-02-25 10:54:00 -0500

    Add All Goods data

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,26 @@
 const fs = require('fs');
+const {parse} = require("@vue/compiler-sfc");
+
+function pagination(pageSize, currentPage, arr) {
+  let skipNum = (currentPage - 1) * pageSize;
+  return (skipNum + pageSize >= arr.length) ? arr.slice(skipNum, arr.length) : arr.slice(skipNum, skipNum = pageSize);
+}
+
+function sortBy(attr, rev) {
+  if (rev === undefined) rev = 1;
+  else rev = rev ? 1 : -1;
+  return function (a, b) {
+    a = a[attr];
+    b = b[attr];
+    if (a < b) return rev * -1;
+    if (a > b) return rev * 1;
+    return 0;
+  }
+}
+
+function range(arr, gt, lte) {
+  return arr.filter(item => item.salePrice >= gt && item.salePrice <= lte);
+}
 
 module.exports = {
   devServer: {
@@ -10,6 +32,43 @@ module.exports = {
           }
         })
       });
+
+      devServer.app.get('/api/goods/allGoods', (req, res) => {
+        const page = parseInt(req.query.page);
+        const size = parseInt(req.query.size);
+        const sort = parseInt(req.query.sort);
+        const gt = parseInt(req.query.priceGt);
+        const lte = parseInt(req.query.priceLte);
+        const cid = req.query.cid;
+        let newData = [];
+        fs.readFile('./db/allGoods.json', 'utf8', (error, data) => {
+          let { result } = JSON.parse(data);
+          let allData = result.data;
+          newData = pagination(size, page, allData);
+
+          if (sort === 1) {
+            newData = newData.sort(sortBy('salePrice', true));
+          } else if (sort === -1) {
+            newData = newData.sort(sortBy('salePrice', false));
+          }
+
+          if (gt && lte) {
+            newData = range(newData, gt, lte);
+          }
+
+          if (newData.length < size) {
+            res.json({
+              data: newData,
+              total: newData.length
+            });
+          } else {
+            res.json({
+              data: newData,
+              total: allData.length
+            })
+          }
+        })
+      })
     }
   }
 }
